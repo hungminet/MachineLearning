d3b861e3545517ea1c74d6c2735b6e52190f0e3b
Nguyễn Trường An <61225076+hungminet@users.noreply.github.com>
2020-09-23 15:28:07 +0700

    power_0

--- Multiple Linear Regression/Multiple Linear Regression.py
+++ Multiple Linear Regression/Multiple Linear Regression.py
@@ -9,24 +9,30 @@ Created on Wed Sep 23 14:07:31 2020
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-#%%
+#%% LOADING DATA
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+
+#%% ENCODING CATEGORICAL DATA
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+transform_tool = ColumnTransformer(transformers = 
+                                   [('encoder', OneHotEncoder(),[3])],
+                                   remainder = 'passthrough')
+X = np.array(transform_tool.fit_transform(X))
 
 
 
 
 
-#%%
+#%% SPLITTING THE DATASET TO TRAINING SET AND TEST SET
 
-
-
-
-
-
-
-#%%
-
-
-
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size = 0.2, 
+                                                    random_state=1)
 
 
 
