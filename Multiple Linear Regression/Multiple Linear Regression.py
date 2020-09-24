f0716ee01fcf45010246b8b547a869c59fce6b41
Nguyễn Trường An <61225076+hungminet@users.noreply.github.com>
2020-09-24 11:02:37 +0700

    mult

--- Multiple Linear Regression/Multiple Linear Regression.py
+++ Multiple Linear Regression/Multiple Linear Regression.py
@@ -32,20 +32,19 @@ X = np.array(transform_tool.fit_transform(X))
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size = 0.2, 
-                                                    random_state=1)
+                                                    random_state=0)
 
 
+
+
+#%% Build model - training 
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X_train, y_train)
 
 
 #%%
-
-
-
-
-
-
-
-#%%
-
-
+y_pred = model.predict(X_test)
+np.set_printoptions(2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
